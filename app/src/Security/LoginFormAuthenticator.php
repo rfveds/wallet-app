34f9c6878aaaac47e33fe9b6af598e7ecd5969ac
Karol <kkijowski11@gmail.com>
2023-05-22 16:54:10 +0200

    user authentication

---
+++ app/src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * wallet-app.
+ *
+ * (c) Karol Kijowski , 2023
+ */
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+/**
+ * Class LoginFormAuthenticator.
+ */
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
+{
+    use TargetPathTrait;
+
+    /**
+     * Login route.
+     *
+     * @const string
+     */
+    public const LOGIN_ROUTE = 'app_login';
+
+    /**
+     * Default route.
+     *
+     * @const string
+     */
+    public const DEFAULT_ROUTE = 'homepage';
+
+    /**
+     * URL Generator.
+     */
+    private UrlGeneratorInterface $urlGenerator;
+
+    /**
+     * Constructor.
+     *
+     * @param UrlGeneratorInterface $urlGenerator Url generator
+     */
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    /**
+     * Does the authenticator support the given Request?
+     *
+     * If this returns false, the authenticator will be skipped.
+     *
+     * @param Request $request HTTP request
+     *
+     * @return bool Result
+     */
+    public function supports(Request $request): bool
+    {
+        return 'app_login' === $request->attributes->get('_route')
+            && $request->isMethod('POST');
+    }
+
+    /**
+     * Create a passport for the current request.
+     *
+     * The passport contains the user, credentials and any additional information
+     * that has to be checked by the Symfony Security system. For example, a login
+     * form authenticator will probably return a passport containing the user, the
+     * presented password and the CSRF token value.
+     *
+     * You may throw any AuthenticationException in this method in case of error (e.g.
+     * a UserNotFoundException when the user cannot be found).
+     *
+     * @param Request $request HTTP request
+     *
+     * @return Passport Passport
+     *
+     * @throws AuthenticationException
+     */
+    public function authenticate(Request $request): Passport
+    {
+        $email = $request->request->get('email', '');
+
+        $request->getSession()->set(Security::LAST_USERNAME, $email);
+
+        return new Passport(
+            new UserBadge($email),
+            new PasswordCredentials($request->request->get('password', '')),
+            [
+                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
+            ]
+        );
+    }
+
+    /**
+     * Called when authentication executed and was successful!
+     *
+     * This should return the Response sent back to the user, like a
+     * RedirectResponse to the last page they visited.
+     *
+     * If you return null, the current request will continue, and the user
+     * will be authenticated. This makes sense, for example, with an API.
+     *
+     * @param Request        $request      HTTP request
+     * @param TokenInterface $token        Token
+     * @param string         $firewallName Firewall name
+     *
+     * @return Response|null HTTP response
+     *
+     * @throws \Exception
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        return new RedirectResponse($this->urlGenerator->generate(self::DEFAULT_ROUTE));
+    }
+
+    /**
+     * Get login URL.
+     *
+     * @param Request $request HTTP request
+     *
+     * @return string Login URL
+     */
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+}
