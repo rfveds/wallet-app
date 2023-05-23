59dc1db90d4d5aabf8cf2d15418900fe1a782d6e
Karol <kkijowski11@gmail.com>
2023-05-23 20:31:58 +0200

    updated category service for gedmo

--- app/src/Service/CategoryService.php
+++ app/src/Service/CategoryService.php
@@ -62,12 +62,6 @@ class CategoryService implements CategoryServiceInterface
      */
     public function save(Category $category): void
     {
-        if (null === $category->getId()) {
-            $category->setCreatedAt(new \DateTimeImmutable());
-        }
-
-        $category->setUpdatedAt(new \DateTimeImmutable());
-
         $this->categoryRepository->save($category);
     }
 
