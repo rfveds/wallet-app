94c861921d51c741b24c550704e98e30224a28b7
Karol <kkijowski11@gmail.com>
2023-05-25 21:35:14 +0200

    caegory service tests

--- app/tests/Service/CategoryServiceTest.php
+++ app/tests/Service/CategoryServiceTest.php
@@ -1,8 +1,159 @@
 <?php
+/**
+ * wallet-app.
+ *
+ * (c) Karol Kijowski , 2023
+ */
 
 namespace App\Tests\Service;
 
-class CategoryServiceTest
-{
+use App\Entity\Category;
+use App\Service\CategoryService;
+use App\Service\CategoryServiceInterface;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\ORMException;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
+/**
+ * Class CategoryServiceTest.
+ */
+class CategoryServiceTest extends KernelTestCase
+{
+    /**
+     * Category repository.
+     */
+    private ?EntityManagerInterface $entityManager;
+
+    /**
+     * Category service.
+     */
+    private ?CategoryServiceInterface $categoryService;
+
+    /**
+     * Set up test.
+     *
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function setUp(): void
+    {
+        $container = static::getContainer();
+        $this->entityManager = $container->get('doctrine.orm.entity_manager');
+        $this->categoryService = $container->get(CategoryService::class);
+    }
+
+    /**
+     * Test save.
+     *
+     * @throws ORMException
+     */
+    public function testSave(): void
+    {
+        // given
+        $expectedCategory = new Category();
+        $expectedCategory->setTitle('Test Category');
+
+        // when
+        $this->categoryService->save($expectedCategory);
+
+        // then
+        $expectedCategoryId = $expectedCategory->getId();
+        $resultCategory = $this->entityManager->createQueryBuilder()
+            ->select('category')
+            ->from(Category::class, 'category')
+            ->where('category.id = :id')
+            ->setParameter(':id', $expectedCategoryId, Types::INTEGER)
+            ->getQuery()
+            ->getSingleResult();
+
+        $this->assertEquals($expectedCategory, $resultCategory);
+    }
+
+    /**
+     * Test delete.
+     *
+     * @throws ORMException
+     */
+    public function testDelete(): void
+    {
+        // given
+        $categoryToDelete = new Category();
+        $categoryToDelete->setTitle('Test Delete Category');
+        $categoryToDelete->setCreatedAt(new \DateTimeImmutable('now'));
+        $categoryToDelete->setUpdatedAt(new \DateTimeImmutable('now'));
+        $this->entityManager->persist($categoryToDelete);
+        $this->entityManager->flush();
+        $deletedCategoryId = $categoryToDelete->getId();
+
+        // when
+        $this->categoryService->delete($categoryToDelete);
+
+        // then
+        $resultCategory = $this->entityManager->createQueryBuilder()
+            ->select('category')
+            ->from(Category::class, 'category')
+            ->where('category.id = :id')
+            ->setParameter(':id', $deletedCategoryId, Types::INTEGER)
+            ->getQuery()
+            ->getOneOrNullResult();
+
+        $this->assertNull($resultCategory);
+    }
+
+    /**
+     * Test find by id.
+     *
+     * @throws NonUniqueResultException
+     */
+    public function testFindById(): void
+    {
+        // given
+        $expectedCategory = new Category();
+        $expectedCategory->setTitle('Test Find By Id Category');
+        $expectedCategory->setCreatedAt(new \DateTimeImmutable('now'));
+        $expectedCategory->setUpdatedAt(new \DateTimeImmutable('now'));
+        $this->entityManager->persist($expectedCategory);
+        $this->entityManager->flush();
+        $expectedCategoryId = $expectedCategory->getId();
+
+        // when
+        $resultCategory = $this->categoryService->findOneById($expectedCategoryId);
+
+        // then
+        $this->assertEquals($expectedCategory, $resultCategory);
+    }
+
+    /**
+     * Test pagination.
+     */
+    public function testCreatePaginatedList(): void
+    {
+        // given
+        $page = 1;
+        $dataSetSize = 15;
+        $expectedResultSize = 10;
+
+        $counter = 0;
+        while ($counter < $dataSetSize) {
+            $category = new Category();
+            $category->setTitle('Test Category #'.$counter);
+            $category->setCreatedAt(new \DateTimeImmutable('now'));
+            $category->setUpdatedAt(new \DateTimeImmutable('now'));
+            $this->categoryService->save($category);
+
+            ++$counter;
+        }
+
+        // when
+        $result = $this->categoryService->createPaginatedList($page);
+
+        // then
+        $this->assertEquals($expectedResultSize, $result->count());
+    }
+
+    // other tests for paginated list
 }
