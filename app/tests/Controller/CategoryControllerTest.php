6c4027eb441a8be902a282d5c34a06b84d7fa12e
Karol <kkijowski11@gmail.com>
2023-05-25 23:45:44 +0200

    category controller tests

--- app/tests/Controller/CategoryControllerTest.php
+++ app/tests/Controller/CategoryControllerTest.php
@@ -5,6 +5,101 @@
 
 namespace App\Tests\Controller;
 
-class CategoryControllerTest
+use App\Entity\Enum\UserRole;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class CategoryControllerTest.
+ */
+class CategoryControllerTest extends WebTestCase
 {
+    /**
+     * Test route /category.
+     *
+     * @const string
+     */
+    public const TEST_ROUTE = '/category';
+
+    /**
+     * Test client.
+     */
+    private KernelBrowser $httpClient;
+
+    /**
+     * Set up test.
+     */
+    protected function setUp(): void
+    {
+        $this->httpClient = static::createClient();
+    }
+
+    /**
+     * Test index route for anonymous user.
+     */
+    public function testIndexRouteAnonymousUser(): void
+    {
+        // given
+        $expectedStatusCode = 302;
+
+        // when
+        $this->httpClient->request('GET', self::TEST_ROUTE);
+        $statusCode = $this->httpClient->getResponse()->getStatusCode();
+
+        // then
+        $this->assertEquals($expectedStatusCode, $statusCode);
+    }
+
+    /**
+     * Test index route for user.
+     *
+     * @throws ContainerExceptionInterface|NotFoundExceptionInterface|ORMException|OptimisticLockException
+     */
+    public function testIndexRouteUser(): void
+    {
+        // given
+        $expectedStatusCode = 200;
+        $user = $this->createUser([UserRole::ROLE_USER->value]);
+        $this->httpClient->loginUser($user);
+
+        // when
+        $this->httpClient->request('GET', self::TEST_ROUTE);
+        $resultStatusCode = $this->httpClient->getResponse()->getStatusCode();
+
+        // then
+        $this->assertEquals($expectedStatusCode, $resultStatusCode);
+    }
+
+    /**
+     * Create user.
+     *
+     * @param array $roles User roles
+     *
+     * @return User User entity
+     *
+     * @throws ContainerExceptionInterface|NotFoundExceptionInterface|ORMException|OptimisticLockException
+     */
+    private function createUser(array $roles): User
+    {
+        $passwordHasher = static::getContainer()->get('security.password_hasher');
+        $user = new User();
+        $user->setEmail('user@example.com');
+        $user->setRoles($roles);
+        $user->setPassword(
+            $passwordHasher->hashPassword(
+                $user,
+                'p@55w0rd'
+            )
+        );
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $userRepository->save($user);
+
+        return $user;
+    }
 }
